1af9bd0a21296242cb7671fa80c28d2ef29e1ae9
Ho Yan Leung <hy.leung@gmail.com>
2015-12-15 19:47:30 -0800

    Adds OMF

--- config.symlink/fish/config.fish
+++ config.symlink/fish/config.fish
@@ -1,36 +1,8 @@
-set normal (set_color normal)
-set magenta (set_color magenta)
-set yellow (set_color yellow)
-set green (set_color green)
-set red (set_color red)
-set gray (set_color -o black)
+# Path to Oh My Fish install.
+set -gx OMF_PATH /Users/hyleung/.local/share/omf
 
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /Users/hyleung/.config/omf
 
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
-
-
-function fish_prompt
-  set last_status $status
-
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color normal
-
-  printf '%s ' (__fish_git_prompt)
-
-  set_color normal
-end
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
